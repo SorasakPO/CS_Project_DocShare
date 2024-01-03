c098da7e917b8efc21b17cc138e514824d77aa90
Sorasak PONGSAWAT <sorasak.po@ku.th>
2024-01-03 16:43:33 +0700

    Hello CS Prject

---
+++ src/main/java/ku/cs/csProject/CsProjectApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.csProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cs/csProject/CsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.csProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
